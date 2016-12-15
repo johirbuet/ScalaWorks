00f75850893407353a5ea336256489453329992d
Md Johirul Islam <johirbuet@gmail.com>
2016-12-15 16:12:40 -0600

    String Interpolation

---
+++ src/StringInterPolation.scala
@@ -0,0 +1,9 @@
+
+
+object StringInterPolation {
+  def main(args: Array[String]): Unit = {
+    val bookTitle="Life Story";
+    println("Book Title is ${bookTitle}");
+    println(s"Book Title is ${bookTitle}");
+  }
+}
