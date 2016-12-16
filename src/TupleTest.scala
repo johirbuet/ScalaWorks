8645fd880431fca41c0ab68a48adc4a3f9dc4af6
Md Johirul Islam <johirbuet@gmail.com>
2016-12-16 14:15:13 -0600

    Tuple in Scala

---
+++ src/TupleTest.scala
@@ -0,0 +1,11 @@
+
+
+object TupleTest {
+  def main(args: Array[String]): Unit = {
+    val tuple=(1,false,"Scala");
+    println(tuple);
+    println(tuple._1);
+    println(tuple._2);
+    println(tuple._3);
+  }
+}
